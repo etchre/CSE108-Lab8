3139f6a28ec3c45ab1d3edf2b74b4bc9ecffd54f
ethan <etchre@outlook.com>
2025-04-01 22:44:17 -0700

    added a script file to allow the frontend to access 'general' apis from the backend

---
+++ src/functions/generalAPI.js
@@ -0,0 +1,26 @@
+const generalURI = 'http://127.0.0.1:5000/'
+
+function promiseCallback({promise, setResponse}) {
+  promise.then(response => {
+    if (!response.ok) {
+      console.log("Response status: " + response.status + " " + response.statusText);
+    }
+    return response.json();
+  }).then(data => {
+    setResponse(data)
+  }).catch(err => {
+    console.log(err);
+  })
+}
+
+function seeAllCourses({setResponse}) {
+  const promise = fetch(generalURI+'/classes',
+    {
+      method: 'GET',
+    }
+  )
+
+  promiseCallback({promise, setResponse})
+}
+
+export default {seeAllCourses}
